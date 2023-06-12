c6e687c7ea374682cb18cf6b8ecd2184f6e18aae
evyz <endermainYT2502@gmail.com>
2023-06-12 22:21:30 +0300

    upd

--- src/newsBlock.js
+++ src/newsBlock.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import "./newsBlock.css"
+import "./newsBlock.css";
 import React from "react";
 function NewsBlock() {
-  const [trueBurger, setTrueBurger] = useState(false)
+  const [trueBurger, setTrueBurger] = useState(false);
   return (
     <div
-    className="p-fix w-100 d-fl ali-cent jc-cent"
+      className='p-fix w-100 d-fl ali-cent jc-cent'
       style={{
         top: 10,
         left: 0,
@@ -16,7 +16,7 @@ function NewsBlock() {
       }}
     >
       <div
-      className="br-15 d-fl jc-sp-be al-cent flex-d-row back-FFF hid"
+        className='br-15 d-fl jc-sp-be al-cent flex-d-row back-FFF hid'
         style={{
           width: "1050px",
           height: "100%",
@@ -24,39 +24,53 @@ function NewsBlock() {
         }}
       >
         <h2>News</h2>
-        <div className="d-fl flex-d-row jc-sp-ar"
+
+        <div
+          className={`menu-chapters ${
+            trueBurger ? "active" : ""
+          } d-fl flex-d-row jc-sp-ar`}
           style={{
             width: "35%",
           }}
-          >
-            <h4  onClick={() => {
-              window.location.pathname = "/adminPanel";
-            }}>Admin Panel</h4>
-          <h4
-            onClick={() => {
-              window.location.pathname = "/aboutus";
-            }}
-          >
-            About us
-          </h4>
-          <h4
-            onClick={() => {
-              window.location.pathname = "/";
-            }}
-          >
-            Block
-          </h4>
-          {!localStorage.getItem("nickname") !== undefined && (
+        >
+          <button onClick={() => setTrueBurger(!trueBurger)} className='burger'>
+            <div className='crumb'></div>
+            <div className='crumb'></div>
+            <div className='crumb'></div>
+          </button>
+          <div className='chapters d-fl flex-d-row jc-sp-ar'>
             <h4
               onClick={() => {
-                window.location.pathname = "/login";
+                window.location.pathname = "/adminPanel";
               }}
             >
-              Sign In
+              Admin Panel
             </h4>
-          )}
+            <h4
+              onClick={() => {
+                window.location.pathname = "/aboutus";
+              }}
+            >
+              About us
+            </h4>
+            <h4
+              onClick={() => {
+                window.location.pathname = "/";
+              }}
+            >
+              Block
+            </h4>
+            {!localStorage.getItem("nickname") !== undefined && (
+              <h4
+                onClick={() => {
+                  window.location.pathname = "/login";
+                }}
+              >
+                Sign In
+              </h4>
+            )}
+          </div>
         </div>
-        {trueBurger && <button className="burger">-</button>}
       </div>
     </div>
   );
