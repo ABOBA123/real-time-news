5cc4fd5bc152cfb480fde239bf711f44b609e5a2
evyz <endermainYT2502@gmail.com>
2023-06-25 22:35:06 +0300

    upd

--- src/http/index.js
+++ src/http/index.js
@@ -1,9 +1,35 @@
-import axios from "axios"
-export const register = async({email, password})=>{
-    const {data} = await axios.post("http://localhost:8080/api/user/register", {email, password})
-    return data
-}
-export const login = async({email, password})=>{
-    const {data} = await axios.post("http://localhost:8080/api/user/login", {email, password})
-    return data
-}
+import axios from "axios";
+
+const URL_PATH = "http://localhost:8080/api/";
+
+export const register = async ({ email, password }) => {
+  const { data } = await axios.post(URL_PATH + "user/register", {
+    email,
+    password,
+  });
+  return data;
+};
+export const login = async ({ email, password }) => {
+  const { data } = await axios.post(URL_PATH + "user/login", {
+    email,
+    password,
+  });
+  return data;
+};
+
+export const getNewsFromApi = async ({ page, tags }) => {
+  let reqStr = "";
+  if (page || tags) {
+    reqStr += "?size=20";
+  }
+  if (page) {
+    reqStr += `page=${page}`;
+  }
+
+  if (tags && tags?.length) {
+    reqStr += `&tags=${JSON.stringify(tags)}`;
+  }
+
+  const { data } = await axios.get(URL_PATH + "news" + reqStr);
+  return data;
+};
